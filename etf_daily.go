c39c2884748e9718b9ba45747bcf3c487be08e13
lwch <lwch748@gmail.com>
2025-11-30 14:04:25 +0800

    etfdaily改名为funddaily

--- etf_daily.go
+++
@@ -1,8 +0,0 @@
-// https://tushare.pro/document/2?doc_id=127
-
-package tushare
-
-// ETFDaily 获取ETF每日行情
-func (cli *Client) ETFDaily(opts ...dailyOpt) ([]DailyTick, error) {
-	return cli.daily("etf_daily", opts...)
-}
